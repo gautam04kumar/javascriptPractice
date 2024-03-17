a9e6915b4ebe072b2ec4f073eb81a58894aab098
gautam <gautam8809240887@gmail.com>
2024-03-18 02:44:13 +0530

    Add editing functionality to task list

---
+++ src/todo/todo.js
@@ -0,0 +1,41 @@
+const inputBox = document.querySelector('#input-box')
+const listContainer = document.querySelector('#list-container')
+function addTask() {
+    if (inputBox.value === '') {
+        alert("Please enter text")
+    } else {
+        let li = document.createElement('li');
+        li.className = 'task-item form-control form-control-sm mb-2'
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li)
+
+        let editButton = document.createElement('button')
+        let deleteButton = document.createElement('button')
+
+        editButton.textContent = 'Edit'
+        editButton.className = 'btn btn-warning mx-2'
+
+
+        deleteButton.textContent = 'Delete'
+        deleteButton.className = 'btn btn-danger'
+
+        let buttonDiv = document.createElement('div');
+        buttonDiv.appendChild(editButton);
+        buttonDiv.appendChild(deleteButton);
+        li.appendChild(buttonDiv);
+
+
+
+    }
+}
+
+listContainer.addEventListener("click",function(e){
+if(e.target.textContent==='Delete'){
+    e.target.parentElement.parentElement.remove()
+}
+else if (e.target.textContent === 'Edit') {
+    
+}
+})
+
+
